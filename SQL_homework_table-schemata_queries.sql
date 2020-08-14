82e933f14df358e75e9d36725fbc90a89ee69b44
gthompson5456 <65741958+gthompson5456@users.noreply.github.com>
2020-08-13 21:03:19 -0400

    Gage's SQL Homework

---
+++ SQL_homework_table-schemata_queries.sql
@@ -0,0 +1,118 @@
+drop table if exists dept_emp;
+
+create table employees (
+	emp_no INT PRIMARY KEY NOT NULL,
+	emp_title_id VARCHAR(35) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(35) NOT NULL,
+	last_name VARCHAR(35) NOT NULL,
+	sex VARCHAR(1)NOT NULL,
+	hire_date DATE NOT NULL
+);
+
+-- Create departments table
+create table departments (
+	dept_no VARCHAR(8) primary key NOT NULL,
+	dept_name VARCHAR(40) NOT NULL
+);
+
+-- Create titles table
+create table titles (
+	title_id VARCHAR(40) NOT NULL,
+	title VARCHAR(40) NOT NULL
+);
+
+-- Create department employee table
+create table dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(8) NOT NULL,
+    foreign key (emp_no) references employees (emp_no),
+    foreign key (dept_no) references departments (dept_no)
+);
+
+
+-- Create salaries table
+create table salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+    foreign key (emp_no) references employees (emp_no)
+);
+
+-- Create Department manager table
+create table dept_manager (
+	dept_no VARCHAR(8) NOT NULL,
+	emp_no INT NOT NULL,
+    foreign key (emp_no) references employees (emp_no),
+    foreign key (dept_no) references departments (dept_no)
+);
+
+SELECT * FROM employees;
+SELECT * FROM departments;
+SELECT * FROM titles;
+SELECT * FROM dept_emp;
+SELECT * FROM salaries;
+SELECT * FROM dept_manager;
+
+-- 1. List the following details of each employee: employee number, last name, first name, gender, and salary
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+JOIN salaries
+ON employees.emp_no = salaries.emp_no;
+
+-- 2. List employees who were hired in 1986.
+SELECT first_name, last_name, hire_date 
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+-- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM departments
+JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no
+JOIN employees
+ON dept_manager.emp_no = employees.emp_no;
+
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no;
+
+--5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+--6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales';
+
+--7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales' 
+OR departments.dept_name = 'Development';
+
+--8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name,
+COUNT(last_name) AS "frequency"
+FROM employees
+GROUP BY last_name
+ORDER BY
+COUNT(last_name) DESC;
+
+
+
+
